f639b26ec745e73526e5d7b35dbaf97662de0ff9
MarcosPauloDosSantosSilva <mpssss@hotmail.com>
2025-05-01 21:53:48 -0300

    Iniciando estudo SpringBoot Loja Virtual.

---
+++ src/main/java/mpss/com/br/LojinhaVirtualEstudoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package mpss.com.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojinhaVirtualEstudoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojinhaVirtualEstudoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mpss/com/br/LojinhaVirtualEstudoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package mpss.com.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojinhaVirtualEstudoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
